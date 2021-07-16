dd72294abd7642b413f25af89424dc5581e514e6
brospresident <andreigaming32@gmail.com>
2021-07-16 20:43:56 +0300

    Update mongo.js

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-admin:U4ySuK46AMLtOAeV@cluster0.kr512.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGO_URL = '';
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connected!');
